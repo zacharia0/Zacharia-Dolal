4e60e5b6a96f914f10b52050c5d1b85170493ed6
Zacharia Dolal <zacharia.dolal@gmail.com>
2025-01-09 18:36:46 -0600

    Added Contact title.

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import styles from "./ContactStyles.module.css"
 const Contact = ()=>{
     return(
         <section id = "contact" className={styles.container}>
-            <h1 className="sectionTitle"></h1>
+            <h1 className="sectionTitle">Contact</h1>
             <form action="https://formspree.io/f/xzzzkqkj" method="post">
                 <div className="formGroup">
                     <label htmlFor="name" hidden>Name</label>
